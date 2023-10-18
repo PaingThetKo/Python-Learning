0a9f5307d4f2184ea4461059130569b96dc78ea0
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-18 16:03:45 +0630

    Area of Circle

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # N=input('Your name is: ');
 # print('Hi '+ N + ' , nice to meet you!');
 
-Age = input('Tell me your age : ');
-print('Your age is ' + Age);
+# Age = input('Tell me your age : ');
+# print('Your age is ' + Age);
+
+π=3.14;
+r=int(input('Radious is: '));
+A=π*r**2
+print(A);
