68808c0821944d906e231126e141bf307cbe3986
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-19 12:00:19 +0630

    if else done homework with username/password

--- if_else.py
+++ if_else.py
@@ -3,8 +3,15 @@
 # elif age>18 and age<45: print('You are normal age, you can work here.');
 # else :                  print('You are old, you can\t work here.');
 
-tired=input('Are You tired? Please answer y (or) n:  ');
-if tired == 'y':    print('Rest well!');
-elif tired == 'n' : print('Go back to work!');
-else:               print('enter only y or n, you idiot!');
+# tired=input('Are You tired? Please answer y (or) n:  ');
+# if tired == 'y':    print('Rest well!');
+# elif tired == 'n' : print('Go back to work!');
+# else:               print('enter only y or n, you idiot!');
+
+username= 'paingthetko';
+password= '123456';
+user = input('Please enter your username : ');
+passw = input('Please enter your password : ');
+if user == username and passw == password : print('Welcome back, Paing Thet Ko! ');
+else : print('Login failed, username or password is incorrect');
 
