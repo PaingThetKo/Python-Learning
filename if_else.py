1a88fc3f74db4aac4b0d29e78b472fd78271352c
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-19 11:58:30 +0630

    if else test with age

---
+++ if_else.py
@@ -0,0 +1,5 @@
+age = int(input('Your age is: '));
+if age<18:              print('You are underage, you can\'t work here.');
+elif age>18 and age<45: print('You are normal age, you can work here.');
+else :                  print('You are old, you can\t work here.');
+
