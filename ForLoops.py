f6a316cd3227cf2dfbb898b001518cb90f41a300
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-19 16:12:38 +0630

    for loop with names

---
+++ ForLoops.py
@@ -0,0 +1,7 @@
+names = ['Kyaw Kyaw', 'Aung Aung', 'Paing Paing', 'Ye Lin'];
+for name in names:
+    if name == 'Paing Paing' : 
+        print(f'{name} is a handsome guy!');
+        break ;
+    else : 
+        print (f'{name} is a nice person');
