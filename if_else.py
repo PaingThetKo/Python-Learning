293fcf3038bced5a6311d2ad1f0438028c260abb
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-19 11:59:18 +0630

    if else test with y/n

--- if_else.py
+++ if_else.py
@@ -1,5 +1,10 @@
-age = int(input('Your age is: '));
-if age<18:              print('You are underage, you can\'t work here.');
-elif age>18 and age<45: print('You are normal age, you can work here.');
-else :                  print('You are old, you can\t work here.');
+# age = int(input('Your age is: '));
+# if age<18:              print('You are underage, you can\'t work here.');
+# elif age>18 and age<45: print('You are normal age, you can work here.');
+# else :                  print('You are old, you can\t work here.');
+
+tired=input('Are You tired? Please answer y (or) n:  ');
+if tired == 'y':    print('Rest well!');
+elif tired == 'n' : print('Go back to work!');
+else:               print('enter only y or n, you idiot!');
 
