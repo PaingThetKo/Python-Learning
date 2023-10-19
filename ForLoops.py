b0a8a96ea9ded22a88520ac0eda1de417125c7c4
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-19 16:13:26 +0630

    for loop with fruits

--- ForLoops.py
+++ ForLoops.py
@@ -1,7 +1,14 @@
-names = ['Kyaw Kyaw', 'Aung Aung', 'Paing Paing', 'Ye Lin'];
-for name in names:
-    if name == 'Paing Paing' : 
-        print(f'{name} is a handsome guy!');
-        break ;
+# names = ['Kyaw Kyaw', 'Aung Aung', 'Paing Paing', 'Ye Lin'];
+# for name in names:
+#     if name == 'Paing Paing' : 
+#         print(f'{name} is a handsome guy!');
+#         break ;
+#     else : 
+#         print (f'{name} is a nice person');
+
+fruits = ['Apple','Orange','Peach','leamon'];
+for fruit in fruits:
+    if fruit == 'Peach' : 
+        print(f'{fruit} is a juicy fruit but looks sussy.');
     else : 
-        print (f'{name} is a nice person');
+        print(f'{fruit} is a good for health.');
