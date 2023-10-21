b85d4050b8241fcabdedbd26565912612a8c8cc6
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-21 10:05:32 +0630

    range in for loop

--- range.py
+++ range.py
@@ -2,5 +2,9 @@
 # for num in nums :
 #     print(num);
 
-for num in range(0,20,4):
-    print(f'{num} Hello!');
+# for num in range(0,20,4):
+#     print(f'{num} Hello!');
+
+pizzas=['cheesy','spicy','mayo chicken','chicken cheesy', 'spicy chicken'];
+for num in range(len(pizzas)):
+    print(pizzas[num]);
