5508d17ce837d915517a2dccb4a194df017f73ff
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-22 09:50:40 +0630

    multi parameter test in function

--- Function.py
+++ Function.py
@@ -1,4 +1,8 @@
-def greet(name):
-    print(f'Good Morning, {name}');
-greet('Paing Thet Ko');
-greet('Kyaw Kyaw');
+# def greet(name):
+#     print(f'Good Morning, {name}');
+# greet('Paing Thet Ko');
+# greet('Kyaw Kyaw');
+
+def greet(time, name):
+    print(f'Good {time}, {name}');
+greet('Morning', 'Paing Thet Ko');
