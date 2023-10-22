93ef43083758ec77d82bff78faa6bf1f54f77eef
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-22 09:52:08 +0630

    default parameter test in function

--- Function.py
+++ Function.py
@@ -3,6 +3,12 @@
 # greet('Paing Thet Ko');
 # greet('Kyaw Kyaw');
 
-def greet(time, name):
+# def greet(time, name):
+#     print(f'Good {time}, {name}');
+# greet('Morning', 'Paing Thet Ko');
+
+def greet(name="Paing Thet Ko",time='Evening'):
     print(f'Good {time}, {name}');
-greet('Morning', 'Paing Thet Ko');
+greet(name='Kyaw Kyaw', time='Morning');
+greet(name='Aung Aung', time='Night');
+
