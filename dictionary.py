28a15efc516fd83e86bec2517cbf12c9713a3889
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-10-22 15:16:06 +0630

    List in dictionary for keys and values

--- dictionary.py
+++ dictionary.py
@@ -7,6 +7,12 @@ person={
 # person['hair']='black';
 # print(person);
 
-if 'name' in person:
-    print('work');
+# if 'name' in person:
+#     print('work');
 
+
+person_keys=list(person.keys());
+print(person_keys);
+
+person_values=list(person.values());
+print(person_values);
