59c4bdcc5cd0545e429e2c105afec422ddde798a
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-04 14:01:54 +0630

    use set function in for loop

--- sorted_set.py
+++ sorted_set.py
@@ -4,5 +4,9 @@
 # names = ['Paing Thet Ko','Aung Aung','kyaw kyaw','aung then','kyaw swar']
 # print(sorted(names))
 
+# nums = [2,8,5,2,1,6,3,4,3,9,7]
+# print(set(nums))
+
 nums = [2,8,5,2,1,6,3,4,3,9,7]
-print(set(nums))
+for num in set(nums):
+    print(num);
