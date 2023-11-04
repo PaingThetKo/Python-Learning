1378992f257b1e170519d31795cfc1a2f3bcefa2
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-04 14:01:22 +0630

    number set function

--- sorted_set.py
+++ sorted_set.py
@@ -1,5 +1,8 @@
 # nums = [2,4,6,8,9,7,6,5,4,3,2,1]
 # print(sorted(nums))
 
-names = ['Paing Thet Ko','Aung Aung','kyaw kyaw','aung then','kyaw swar']
-print(sorted(names))
+# names = ['Paing Thet Ko','Aung Aung','kyaw kyaw','aung then','kyaw swar']
+# print(sorted(names))
+
+nums = [2,8,5,2,1,6,3,4,3,9,7]
+print(set(nums))
