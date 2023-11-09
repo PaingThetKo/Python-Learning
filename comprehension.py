420db3492bee2083198899cb8e27dd7050e0c9e6
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-09 08:53:47 +0630

    prices double in comprehension way

--- comprehension.py
+++ comprehension.py
@@ -1,7 +1,10 @@
 prices = [2000,1000,3000,5000]
 
-double_prices = [];
-for price in prices :
-    double_prices.append(price*2)
+# double_prices = [];
+# for price in prices :
+#     double_prices.append(price*2)
+# print(double_prices)
+
+double_prices = [price*2 for price in prices]
 print(double_prices)
 
