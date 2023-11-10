0366450c7462a9abb1759079f805ba419dc0a84a
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-10 22:13:00 +0630

    decorator concept, function over function

--- decorator.py
+++ decorator.py
@@ -1,3 +1,11 @@
+def greet(fun):
+    def wrapper():
+        print('My name is :')
+        fun()
+        print('Nice to meet you.')
+    return wrapper
+
+@greet
 def sayname():
     print('Paing Thet Ko.')
 sayname()
