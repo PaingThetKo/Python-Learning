1c1ee384cb3940ba6af18098255401d1656307dc
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-10 10:34:25 +0630

    filtering concept with lambda function

--- lambda.py
+++ lambda.py
@@ -9,8 +9,13 @@
 
 
 # # filtering concept 
+# nums = [1,2,3,4,5,6,7,8,9]
+# def filterfuncion(num):
+#     return (num%2)==0
+# nums = list(filter(filterfuncion,nums))
+# print(nums)
+
+# filterig concept with lambda function
 nums = [1,2,3,4,5,6,7,8,9]
-def filterfuncion(num):
-    return (num%2)==0
-nums = list(filter(filterfuncion,nums))
-print(nums)
+evenNum = list (filter(lambda num:(num%2)==0,nums))
+print(evenNum)
