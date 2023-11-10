ebc7c14d516cb26790979a340ff94772025079f1
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-10 10:32:36 +0630

    lambda function

--- lambda.py
+++ lambda.py
@@ -1,4 +1,8 @@
 # normal return function 
-def add(n1,n2):
-    return n1+n2
+# def add(n1,n2):
+#     return n1+n2
+# print(add(4,5))
+
+# lambda function
+add = lambda n1,n2: n1+n2
 print(add(4,5))
