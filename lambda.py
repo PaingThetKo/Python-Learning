898950862a5948193dd8174ec01c0f4de6864601
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-10 10:33:31 +0630

    filtering concept

--- lambda.py
+++ lambda.py
@@ -4,5 +4,13 @@
 # print(add(4,5))
 
 # lambda function
-add = lambda n1,n2: n1+n2
-print(add(4,5))
+# add = lambda n1,n2: n1+n2
+# print(add(4,5))
+
+
+# # filtering concept 
+nums = [1,2,3,4,5,6,7,8,9]
+def filterfuncion(num):
+    return (num%2)==0
+nums = list(filter(filterfuncion,nums))
+print(nums)
