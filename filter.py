ffcef1d993d38dc8ce991a6e0e7273306984aeeb
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-10 07:35:08 +0630

    Filtering way

--- filter.py
+++ filter.py
@@ -8,5 +8,11 @@ nums = [1,2,3,4,5,6,7,8,9,10]
 # print(evenNum)
 
 #      comprehension way
-nums = [num for num in nums if (num%2)==0]
+# nums = [num for num in nums if (num%2)==0]
+# print(nums)
+
+#      filtering way
+def filterFunction(num):
+    return (num%2)==0
+nums = list(filter(filterFunction,nums))
 print(nums)
