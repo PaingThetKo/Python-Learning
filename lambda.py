b66de89977267ded7b117f5b38fcc41c27f483ad
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-10 10:36:28 +0630

    map concept with lambda function

--- lambda.py
+++ lambda.py
@@ -22,8 +22,14 @@
 
 
 # map concept 
+# nums = [1,2,3,4,5,6,7,8,9]
+# def mapFunction(num):
+#     return num*2
+# nums = list (map(mapFunction,nums))
+# print(nums)
+
+
+# map concept with lambda function
 nums = [1,2,3,4,5,6,7,8,9]
-def mapFunction(num):
-    return num*2
-nums = list (map(mapFunction,nums))
-print(nums)
+double_num = list(map(lambda num: num*2,nums))
+print(double_num)
