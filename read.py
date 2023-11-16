3f9632fab22d73a93e059736441e83776dd4323e
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-16 12:45:08 +0630

    read file with readline function and seek function

--- read.py
+++ read.py
@@ -3,4 +3,9 @@ file = open('./text.txt') #open the file
 for line in file: #read with FOR LOOP
     print(line)
 
+file.seek(0) # start read file from start(0), if not, program at end of line and will continue with nothing
+
+linelist = file.readline()
+print(linelist)
+
 file.close() # close file, if not, program performance could reduce
