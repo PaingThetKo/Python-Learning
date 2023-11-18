4ef65068b6d3b7d12297e4597224b435379a808c
LordPaingThetKo <lordpaingthetko@gmail.com>
2023-11-18 16:34:23 +0630

    write data to another file with List function

--- write.py
+++ write.py
@@ -1,5 +1,9 @@
-with open ('./about.txt', 'w') as file:
-    file.write('My name is Paing Thet.')
+# with open ('./about.txt', 'w') as file:
+#     file.write('My name is Paing Thet.')
 
-with open ('./about.txt', 'a') as file:
-    file.write('\nI am 21 years old.')
+# with open ('./about.txt', 'a') as file:
+#     file.write('\nI am 21 years old.')
+
+list = ['My name is Paing Thet Ko.', '\nI am 21 years old.']
+with open ('./about.txt' , 'a') as file :
+    file.writelines(list)
